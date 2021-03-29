dd37d5e5fcc585bbb65158efe4bf62bed59ac94f
Isabelle <izzyspencer98@gmail.com>
2021-03-29 16:07:45 +0100

    added spotify client ID and secret

--- spootify/src/config.js
+++ spootify/src/config.js
@@ -2,7 +2,7 @@ export default {
   api: {
     baseUrl: 'https://api.spotify.com/v1',
     authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: '',
-    clientSecret: ''
+    clientId: '58f6103e7052412598c4b8838fb14988',
+    clientSecret: 'b2a3049e58e94ba19f1af8994efbc1a5'
   }
 }
